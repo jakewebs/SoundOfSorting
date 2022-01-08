c93b6ea121a07149ba57457014fa370596e3b27a
Jake Webster <jakewebs2@gmail.com>
2022-01-08 00:00:43 -0800

    added functionality to step through and visualize sorts nicely

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 let osc, playing, freq, amp;
-
+let colorArr = [];
+let x  = 0;
+let sorted = 0;
 function setup() {
   let cnv = createCanvas(1200, 675);
-  cnv.mousePressed(playOscillator);
-  osc = new p5.Oscillator('sine');
+  //cnv.mousePressed(playOscillator);
+  //osc = new p5.Oscillator('sine');
+  //noLoop();
+  //frameRate(10);
+  noLoop();
 }
 
 function draw() {
@@ -14,13 +19,13 @@ function draw() {
   line(1200,0,1200,675);
   //background(230);
 
-  freq = constrain(map(mouseX, 0, width, 100, 800), 100, 800);
-  amp = constrain(map(mouseY, height, 0, 0, 1), 0, 1);
+  //freq = constrain(map(mouseX, 0, width, 100, 800), 100, 800);
+  //amp = constrain(map(mouseY, height, 0, 0, 1), 0, 1);
 
-  text('tap to play', 20, 20);
-  text('freq: ' + freq, 20, 40);
-  text('amp: ' + amp, 20, 60);
-  if (mouseIsPressed) {
+  //text('tap to play', 20, 20);
+  //text('freq: ' + freq, 20, 40);
+  //text('amp: ' + amp, 20, 60);
+  /*if (mouseIsPressed) {
     let c = color(Math.random() * 256, Math.random() * 256, Math.random() * 256);
     fill(c);
     ellipse(mouseX, mouseY, 80, 80);
@@ -31,16 +36,46 @@ function draw() {
   
   if (playing) {
     // smooth the transitions by 0.1 seconds
-    osc.freq(freq, 0.1);
-    osc.amp(amp, 0.1);
+    //osc.freq(freq, 0.1);
+    //osc.amp(amp, 0.1);
+  }*/
+  background(204);
+  line(x, 0, x, height);
+
+  if (colorArr.length > 0) {
+    for (let i = 0; i < colorArr.length; i++) {
+      let cur = colorArr[i];
+      let curColor = color(2 * cur[0], 2 * cur[1], 2 * cur[2]);
+      fill(curColor);
+      //square(50 + (50*i), 200, 45);
+      rect(50 + (50 * i), 350 - (2 * cur[0]), 45, 50 + (2 * cur[0]));
+    }
   }
+
+  if (sorted == 1) {
+    text('Sorted!', 100, 100);
+  }
+}
+
+function makeArr(length) {
+  let arr = [];
+  for (var i = 0; i < length; i++) {
+      let r = Math.floor(Math.random() * 128);
+      let g = Math.floor(Math.random() * 128);
+      let b = Math.floor(Math.random() * 128);
+      let num = (1000000 * r) + (1000 * g) + b;
+      arr.push(num);
+      colorArr.push([r, g, b]);
+  }
+  sorted = 0;
+  return arr;
 }
 
 function playOscillator() {
   // starting an oscillator on a user gesture will enable audio
   // in browsers that have a strict autoplay policy.
   // See also: userStartAudio();
-  osc.start();
+  //osc.start();
   playing = true;
 }
 
@@ -56,16 +91,70 @@ function getFreq(mag) {
 
 function reset() {
     document.getElementById("stuff").innerHTML = "";
+    redraw();
+    showButtons();
 }
 
 function shuff(arr) {
+    document.getElementById("header").innerHTML = "Choose Your Sort";
+    showButtons();
+    //myFunction();
     for (var i = 0; i < arr.length; i++) {
         let index = Math.floor(Math.random() * arr.length);
         let val = arr[index];
+        let cVal = colorArr[index];
         arr[index] = arr[i];
         arr[i] = val;
+        colorArr[index] = colorArr[i];
+        colorArr[i] = cVal;
+        clear();
     }
+    sorted = 0;
     document.getElementById("stuff").innerHTML = arr;
     console.log(arr);
+    redraw();
     return arr;
+}
+
+//let comparisons = 0;
+
+function sleep(seconds) 
+{
+  var e = new Date().getTime() + (seconds * 1000);
+  console.log("sleeping...");
+  while (new Date().getTime() <= e) {}
+}
+
+/*function mousePressed() {
+  x += 10;
+  redraw();
+}*/
+
+
+function hideButtons(exempt) {
+  let x = [];
+  x.push(document.getElementById("bubble"));
+  x.push(document.getElementById("insertion"));
+  x.push(document.getElementById("quick"));
+  x.push(document.getElementById("merge"));
+  x.push(document.getElementById("radix"));
+  for (var i = 0; i < x.length; i++) {
+    if (x[i].id != exempt) {
+      x[i].style.display = "none";
+    }
+  }
+}
+
+function showButtons() {
+  let x = [];
+  x.push(document.getElementById("bubble"));
+  x.push(document.getElementById("insertion"));
+  x.push(document.getElementById("quick"));
+  x.push(document.getElementById("merge"));
+  x.push(document.getElementById("radix"));
+  for (var i = 0; i < x.length; i++) {
+    if (x[i].style.display === "none") {
+      x[i].style.display = "initial";
+    }
+  }
 }
--- sorts.js
+++ sorts.js
@@ -1,4 +1,7 @@
+
+
 let comparisons = 0;
+/*
 function bubbleSort(nums) {
     comparisons = 0;
     for (var i = 0; i < nums.length; i++) {
@@ -17,10 +20,42 @@ function bubbleSort(nums) {
     console.log(nums);
     console.log(comparisons);
     document.getElementById("stuff").innerHTML = nums;
+}*/
+function bubbleSort(nums) {
+    document.getElementById("header").innerHTML = "Bubble Sort Selected";
+    hideButtons("bubble");
+    comparisons = 0;
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = 0; j < nums.length - i - 1; j++) {
+            cur = nums[j];
+            next = nums[j + 1];
+            //mag = Math.abs(cur - next);
+            comparisons++;
+            //tone.generateSound(mag);
+            if (cur > next) {
+                let temp = colorArr[j + 1];
+                colorArr[j + 1] = colorArr[j];
+                colorArr[j] = temp;
+                nums[j] = next;
+                nums[j + 1] = cur;
+                //clear();
+                //sleep(.1);
+                redraw();
+                return;
+            }
+        }
+    }
+    // if we make it here we are done sorting
+    sorted = 1;
+    console.log(nums);
+    console.log(comparisons);
+    document.getElementById("stuff").innerHTML = nums;
 }
 
 
 function insertionSort(nums) {
+    document.getElementById("header").innerHTML = "Insertion Sort Selected";
+    hideButtons("insertion");
     comparisons = 0;
     let min = nums[0];
     let index = 0;
@@ -43,7 +78,12 @@ function insertionSort(nums) {
             let mag = Math.abs(nums[j] - next);
             nums[j - 1] = nums[j];
             nums[j] = next;
+            let cNext = colorArr[j - 1];
+            colorArr[j - 1] = colorArr[j];
+            colorArr[j] = cNext;
+            redraw();
             j--;
+            return;
         }
     }
 
@@ -52,7 +92,11 @@ function insertionSort(nums) {
     document.getElementById("stuff").innerHTML = nums;
 }
 
+
+// may need to do iterative mergesort to work for my vis?
 function mergeSort(nums) {
+    document.getElementById("header").innerHTML = "Merge Sort Selected";
+    hideButtons("merge");
     comparisons = 0;
     nums = ms(nums);
     console.log(comparisons);
@@ -73,7 +117,7 @@ function ms(nums) {
 
 function merge(first, second) {
     let arr = [];
-    while (first.length  && second.length) {
+    while (first.length && second.length) {
         comparisons++;
         if (first[0] < second[0]) {
             arr.push(first.shift());
@@ -86,7 +130,10 @@ function merge(first, second) {
     return [...arr, ...first, ...second];
 }
 
+// may need to do iterative quicksort to work for my vis?
 function quickSort(nums) {
+    document.getElementById("header").innerHTML = "Quick Sort Selected";
+    hideButtons("quick");
     comparisons = 0;
     qs(nums, 0, nums.length - 1);
     console.log(comparisons);
@@ -115,23 +162,37 @@ function partition(nums, left, right) {
             let temp = nums[j];
             nums[j] = nums[i];
             nums[i] = temp;
+            let cTemp = colorArr[j];
+            colorArr[j] = colorArr[i];
+            colorArr[i] = cTemp;
+            //redraw();
+            //return;
         }
     }
     let temp = nums[i + 1];
     nums[i + 1] = nums[right];
     nums[right] = temp;
+    let cTemp = colorArr[i + 1];
+    colorArr[i + 1] = colorArr[right];
+    colorArr[right] = cTemp;
+    //redraw();
     return i + 1; 
 }
 
+let d = 0;
 function LSDRadixSort(nums) {
+    document.getElementById("header").innerHTML = "LSD Radix Sort Selected";
+    hideButtons("radix");
     const m = getMax(nums);
     const D = (int) (Math.log(m + 1) / Math.log(4));
     let temp = [];
-    let d = 0;
+    let tempColor = [];
+    //let d = 0;
     while (d < D) {
         let count = [];
         for (var i = 0; i < 10; i++) {
             count.push(0);
+            tempColor.push([0, 0, 0]);
         }
         for (var i = 0; i < nums.length; i++) {
             count[(int)(nums[i] / (Math.pow(10, d))) % 10]++;
@@ -142,14 +203,20 @@ function LSDRadixSort(nums) {
         }
         for (var i = nums.length - 1; i >= 0; i--) {
             temp[count[(int)(nums[i] / (Math.pow(10, d))) % 10] - 1] = nums[i];
+            tempColor[count[(int)(nums[i] / (Math.pow(10, d))) % 10] - 1] = colorArr[i];
             count[(int)(nums[i] / (Math.pow(10, d))) % 10]--;
         }
         for (var i = 0; i < nums.length; i++) {
             //tone.generateSound(temp[i]);
             nums[i] = temp[i];
+            colorArr[i] = tempColor[i];
         }
         d++;
+        redraw();
+        //console.log(d);
+        return;
     }
+    d = 0;
     console.log(nums);
     document.getElementById("stuff").innerHTML = nums;
 }
